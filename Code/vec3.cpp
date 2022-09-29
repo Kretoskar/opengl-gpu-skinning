2d106b57326f33a3240d4bad43bb5563fd6290e6
Oskar Kogut <kogutoskar123@gmail.com>
2022-09-29 21:50:36 +0200

    Add vector3

---
+++ Code/vec3.cpp
@@ -0,0 +1,81 @@
+﻿#include "vec3.h"
+#include <corecrt_math.h>
+
+#define VEC3_EPSILON 0.000001f
+#define RAD_TO_DEG 57.2958f
+
+vec3 vec3::operator+(const vec3& v1, const vec3& v2)
+{
+    return vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+vec3 vec3::operator-(const vec3& v1, const vec3& v2)
+{
+    return vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+vec3 vec3::operator*(const vec3& v, float f)
+{
+    return vec3(v.x * f, v.y * f, v.z * f);
+}
+
+vec3 vec3::operator*(const vec3& v1, const vec3& v2)
+{
+    return vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+
+float vec3::Dot(const vec3& v1, const vec3& v2)
+{
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+float vec3::LenSq(const vec3& v)
+{
+    return v.x * v.x + v.y * v.y + v.z * v.z; 
+}
+
+float vec3::Len(const vec3& v)
+{
+    float lenSq = LenSq(v);
+
+    if(lenSq < VEC3_EPSILON)
+    {
+        return 0.0f;
+    }
+
+    return sqrtf(lenSq);
+}
+
+void vec3::Normalize(vec3& v)
+{
+    float lenSq = LenSq(v);
+    if(lenSq < VEC3_EPSILON) { return; }
+    float invLen = 1.0f / sqrtf(lenSq);
+    v.x *= invLen;
+    v.y *= invLen;
+    v.z *= invLen;
+}
+
+vec3 vec3::Normalized(const vec3& v)
+{
+    float lenSq = LenSq(v);
+    if(lenSq < VEC3_EPSILON) { return v; }
+    float invLen = 1.0f / sqrtf(lenSq);
+    return vec3(
+        v.x * invLen,
+        v.y * invLen,
+        v.z * invLen
+    );
+}
+
+float vec3::Angle(const vec3& v1, const vec3& v2)
+{
+    return acosf(Dot(v1, v2) / (Len(v1) * Len(v2)));
+}
+
+float vec3::AngleDeg(const vec3& v1, const vec3& v2)
+{
+    return Angle(v1, v2) * RAD_TO_DEG;
+}
+
+
---
+++ Code/vec3.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+struct vec3
+{
+    // allows data to be accessed either by .x, .y, .z or .v array
+    union
+    {
+        struct 
+        {
+            float x;
+            float y;
+            float z;
+        };
+        float v[3];
+    };
+
+    vec3() : x(0.0f), y(0.0f), z(0.0f) {}
+    vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+    vec3(float *fv) : x(fv[0]), y(fv[1]), z(fv[2]) {}
+
+    vec3 operator+ (const vec3 &v1, const vec3 &v2);
+    vec3 operator- (const vec3 &v1, const vec3 &v2);
+    vec3 operator* (const vec3 &v, float f);
+    vec3 operator* (const vec3 &v1, const vec3 &v2);
+    static float Dot(const vec3 &v1, const vec3 &v2);
+    static float LenSq(const vec3& v);
+    static float Len(const vec3 &v);
+    static void Normalize (vec3 &v);
+    static vec3 Normalized(const vec3 &v);
+    static float Angle(const vec3 &v1, const vec3 &v2);
+    static float AngleDeg(const vec3 &v1, const vec3 &v2);
+};
