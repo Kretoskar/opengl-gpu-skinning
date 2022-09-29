c202844b68b9bca484b468c3cb31e90de9f89b77
Oskar Kogut <kogutoskar123@gmail.com>
2022-09-30 00:11:59 +0200

    Add templated vec4 and vec2

---
+++ Code/TVec2.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+template<typename T>
+struct TVec2
+{
+    union
+    {
+        struct
+        {
+            T x;
+            T y;
+        };
+        T v[2];
+    };
+
+    TVec2() : x(T(0)), y(T(0)) {}
+    TVec2(T x, T y) : x(x), y(y) {}
+    TVec2(T* fv) : x(fv[0]), y(fv[1]) {}
+};
+
+typedef TVec2<float> Vec2;
+typedef TVec2<int> IVec2;
+typedef TVec2<unsigned int> UIVec2;
---
+++ Code/TVec4.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+template<typename T>
+struct TVec4
+{
+    union
+    {
+        struct
+        {
+            T x;
+            T y;
+            T z;
+            T w;
+        };
+        T v[4];
+    };
+
+    TVec4<T>() : x(T(0)), y(T(0)), z(T(0)), w(T(0)) {}
+    TVec4<T>(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+    TVec4<T>(T* fv) : x(fv[0]), y(fv[1]), z(fv[2]), w(fv[3]) {}
+};
+
+typedef TVec4<float> Vec4;
+typedef TVec4<int> IVec4;
+typedef TVec4<unsigned int> UIVec4;
