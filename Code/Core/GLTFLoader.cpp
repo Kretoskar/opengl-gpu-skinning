8d8db49cd895f219aa286cbfe3cd6d30d907d428
Oskar Kogut <kogutoskar123@gmail.com>
2023-01-20 18:25:22 +0100

    Add CGLTF and CGLTF loader

---
+++ Code/Core/GLTFLoader.cpp
@@ -0,0 +1,47 @@
+﻿#include "GLTFLoader.h"
+
+#include <cstring>
+#include <iostream>
+
+cgltf_data* LoadGLTFFile(const char* path)
+{
+    constexpr cgltf_options options = {};
+    cgltf_data* data = nullptr;
+    cgltf_result result = cgltf_parse_file(&options, path, &data);
+
+    if(result != cgltf_result_success)
+    {
+        std::cout << "Could not load: " << path << "\n";
+        return nullptr;
+    }
+
+    result = cgltf_load_buffers(&options,  data, path);
+    if(result != cgltf_result_success)
+    {
+        cgltf_free(data);
+        std::cout << "Could not load: " << path << "\n";
+        return nullptr;
+    }
+
+    result = cgltf_validate(data);
+    if(result != cgltf_result_success)
+    {
+        cgltf_free(data);
+        std::cout << "Could not load: " << path << "\n";
+        return nullptr;
+    }
+
+    return data;
+}
+
+void FreeGLTFFile(cgltf_data* data)
+{
+    if(data == nullptr)
+    {
+        std::cout << "WARNING: Can't free null data\n";
+    }
+    else
+    {
+        cgltf_free(data);
+    }
+}
---
+++ Code/Core/GLTFLoader.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#ifndef _H_GLTFLOADER_
+#define _H_GLTFLOADER_
+
+#include "cgltf.h"
+
+cgltf_data* LoadGLTFFile(const char* path);
+void FreeGLTFFile(cgltf_data* data);
+
+#endif
